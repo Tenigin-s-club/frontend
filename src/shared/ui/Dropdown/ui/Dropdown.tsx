7f1f9b928124c140b835969317295f410066582b
EgorDyach <tvoysosed33@gmail.com>
2024-10-26 06:06:46 +0300

    added dropdown

--- src/shared/ui/Dropdown/ui/Dropdown.tsx
+++ src/shared/ui/Dropdown/ui/Dropdown.tsx
@@ -1,5 +1,9 @@
 import { useRef, useEffect, useState, ChangeEvent } from "react";
-
+import Input from "../../Input";
+import style from "./Dropdown.module.scss";
+import { useDebounce } from "@/shared/hooks/useDebounce";
+import { EmptyDropdown } from "./EmptyDropdown";
+import MapMarker from "@/shared/assets/MapMarker.svg";
 interface DropdownProps<T> {
   options: T[];
   selectedOption: T;
@@ -11,18 +15,39 @@ const Dropdown = ({
   selectedOption,
   setSelectedOption,
 }: DropdownProps<string>) => {
+  const [inputValue, setInputValue] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const value = useDebounce(inputValue, 300);
+
+  useEffect(() => {
+    setSelectedOption(value);
+    if (inputValue.length > 2 && isEditing) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    setSelectedOption(value);
+  }, [isEditing]);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOptionClick = (option: string) => {
-    setSelectedOption(option);
+    setIsEditing(false);
+    setInputValue(option);
     setIsOpen(false);
   };
 
+  const inputRef = useRef<HTMLInputElement>(null);
   const dropdownRef = useRef<HTMLUListElement>(null);
   const handleClickOutside = (e: MouseEvent) => {
     if (
       dropdownRef.current &&
-      !dropdownRef.current.contains(e.target as Node)
+      !dropdownRef.current.contains(e.target as Node) &&
+      inputRef.current &&
+      !inputRef.current.contains(e.target as Node)
     ) {
       setIsOpen(false);
     }
@@ -36,37 +61,35 @@ const Dropdown = ({
   }, []);
 
   const handleClick = (e: ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(e.target.value);
-    console.log(selectedOption, e.target.value);
-    if (selectedOption.length > 2) {
-      setIsOpen(true);
-    } else {
-      setIsOpen(false);
-    }
+    setIsEditing(true);
+    setInputValue(e.target.value);
   };
 
   return (
-    <div style={{ position: "relative" }}>
-      <input value={selectedOption} onChange={handleClick} />
+    <div style={{ position: "relative", maxWidth: 400 }}>
+      <Input ref={inputRef} value={inputValue} onChange={handleClick} />
       {isOpen && (
-        <ul ref={dropdownRef}>
-          {options
-            .filter((el) =>
-              el.toLowerCase().includes(selectedOption.toLowerCase())
-            )
-            .map((option, index) => (
-              <li
-                key={index}
-                onClick={() => handleOptionClick(option)}
-                style={{
-                  cursor: "pointer",
-                  padding: 5,
-                  fontSize: 18,
-                }}
-              >
-                {option}
-              </li>
-            ))}
+        <ul className={style.Dropdown} ref={dropdownRef}>
+          {!!options.filter((el) =>
+            el.toLowerCase().includes(selectedOption.toLowerCase())
+          ).length &&
+            options
+              .filter((el) =>
+                el.toLowerCase().includes(selectedOption.toLowerCase())
+              )
+              .map((option, index) => (
+                <li
+                  key={index}
+                  onClick={() => handleOptionClick(option)}
+                  className={style.DropdownItem}
+                >
+                  <MapMarker />
+                  <span>{option}</span>
+                </li>
+              ))}
+          {!options.filter((el) =>
+            el.toLowerCase().includes(selectedOption.toLowerCase())
+          ).length && <EmptyDropdown />}
         </ul>
       )}
     </div>
